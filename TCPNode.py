48655b82ae3da13ced00c432f09940bf37084ee9
Fabian Leandro Flores <b53779@ecci.ucr.ac.cr>
2018-09-17 16:13:35 -0600

    Finished the encoding and decoding of messages on both ends of the TCPNodes.

--- TCPNode.py
+++ TCPNode.py
@@ -37,11 +37,15 @@ class TCPNode:
                 print('[NODE] RECEIVED A MESSAGE WITH', length, 'TRIPLETS:')
 
                 for i in range(0, length):
-                    # struct
+                    # Read the first 5 bytes, which contain the address and mask.
+                    # Each triplet has 8 bytes, so 3 bytes remain.
                     message = conn.recv(self.TRIPLET_SIZE-3)
                     triplets = struct.unpack('!BBBBB', message)
-                    print('[NODE] ADDRESS: ', triplets[i], '.', triplets[1], '.', triplets[2], '.', triplets[3],
-                          ', SUBNET MASK: ', triplets[4], ', COST: ', int(conn.recv(3)), sep='')
+
+                    # Read the last 3 bytes (representing the cost), and interpret them as an int.
+                    cost = int.from_bytes(conn.recv(3), byteorder='big', signed=False)
+                    print('[NODE] ADDRESS: ', triplets[0], '.', triplets[1], '.', triplets[2], '.', triplets[3],
+                          ', SUBNET MASK: ', triplets[4], ', COST: ', cost, sep='')
 
     def send_message(self, ip, port, message):
         print("[NODE] SENDING ", len(message), " BYTES TO ", ip, ":", port)
@@ -65,14 +69,13 @@ class TCPNode:
     def read_message(self):
         length = int(input("[NODE] Enter the length of your message...\n"))
 
-        # The message wiull be 2 bytes (length) + the size of each triplet * the amount of triplets
-        message = bytearray(length*self.TRIPLET_SIZE+2)
-        cost = bytearray(4)
+        # The message will be 2 bytes (which represent the length) + the size of each triplet * the amount of triplets
+        message = bytearray(2+length*self.TRIPLET_SIZE)
 
         # The first 2 bytes of the message (H is 2 bytes) should be the length of triplets in the message
         struct.pack_into("!H", message, 0, length)
 
-        # Counts bytes already written, in order to add information in the right location
+        # Counts bytes already written, in order to add information in the right location of the message buffer
         offset = 2
 
         # Then add 8 bytes per triplet (4 for IP, 1 for mask, 3 for cost)
@@ -82,17 +85,18 @@ class TCPNode:
                 .strip().split(' ')
             address = current_message[0].strip().split('.')
 
-            # Each triplet is coded with the following 8-byte format:
+            # Each triplet is encoded with the following 8-byte format:
             # BBBB (4 bytes) network address
-            # B    (1 byte) subnet mask
+            # B    (1 byte)  subnet mask
             # I    (4 bytes) cost. The cost should only be 3 bytes, this is handled below.
             struct.pack_into('!BBBBB', message, offset, int(address[0]), int(address[1]), int(address[2]), int(address[3]), int(current_message[1]))
 
             # Pack the cost into a 4 byte buffer
             cost = struct.pack('!I', int(current_message[2]))
 
-            # Write the cost into the message buffer, dropping the most significant byte
-            message.extend(cost[1:3])
+            # Write the cost into the message buffer, copying only 3 bytes
+            # The least significant byte is the one dropped because its encoded as big endian
+            message[offset+5:offset+8] = cost[1:]
 
             # Move the offset to write the next triplet
             offset += self.TRIPLET_SIZE
