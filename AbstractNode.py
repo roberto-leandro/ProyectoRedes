8eb6266f0c6133fbb3353dd3eddeffe3d222298b
Carlos Azofeifa <cazofeifa35@gmail.com>
2018-09-21 12:23:25 -0600

    Refactor input validation, add useful input feedback

--- AbstractNode.py
+++ AbstractNode.py
@@ -1,6 +1,5 @@
 import threading
 import struct
-import sys
 import socket
 from abc import ABC, abstractmethod
 
@@ -8,7 +7,6 @@ try:
     import readline
 except ImportError:
     pass
-
 
 class AbstractNode(ABC):
     HEADER_SIZE = 2
@@ -55,7 +53,9 @@ class AbstractNode(ABC):
 
             elif command[0] == "sendMessage":
                 message = self.read_and_encode_message()
-                self.send_message(ip=command[1], port=int(command[2]), message=message)
+                self.send_message(ip=command[1],
+                                  port=int(command[2]),
+                                  message=message)
 
             elif command[0] == "exit" or command[0] == "deleteNode":
                 self.stop_node()
@@ -91,12 +91,69 @@ class AbstractNode(ABC):
 
             self.reachability_table_lock.release()
 
+    @staticmethod
+    def __get_valid_message_input():
+        def get_input():
+            new_input = input("Type the message to be sent as follows:\n" +
+                              "<IP address> <subnet mask> <cost>\n")
+            new_input = new_input.strip().split(' ')
+            return new_input
+
+        while True:
+            message = get_input()
+            if len(message) != 3:
+                print("Wrong number of tokens, expected 3 tokens with the format:")
+                print("x.x.x.x x x     Where x is a positive integer")
+                continue
+
+            address_token_list = message[0].strip().split('.')
+            try:
+                address_token_list = [int(tok) for tok in address_token_list]
+                net_mask = int(message[1])
+                cost = int(message[2])
+            except ValueError:
+                print("Unexpected input, expected input with format:")
+                print("x.x.x.x x x     Where x is a positive integer")
+                continue
+
+            if len(address_token_list) != 4:
+                print("Invalid address, expected address with format: x.x.x.x")
+                continue
+
+            is_address_valid = True
+            for token in address_token_list:
+                if token > 255:
+                    print("Invalid address token, must be less than 255")
+                    is_address_valid = False
+                    break
+                elif token < 0:
+                    print("Invalid address token, must be a positive integer")
+                    is_address_valid = False
+                    break
+            if not is_address_valid:
+                continue
+
+            if net_mask > 32:
+                print("Invalid network mask, expected less than 32")
+                continue
+            elif net_mask < 0:
+                print("Network mask must be a positive integer")
+                continue
+
+            if cost < 0:
+                print("Invalid cost, must be a positive integer")
+                continue
+            elif cost > 0xFF_FF_FF:  # max three bytes
+                print("Invalid cost, max number exceeded")
+                continue
+
+            return address_token_list, net_mask, cost
+
     def read_and_encode_message(self):
         length = 0
         while not length > 0:
             try:
                 length = int(input("Enter the length of your message...\n"))
-
             except ValueError:
                 print("Please enter a valid integer.")
 
@@ -106,40 +163,7 @@ class AbstractNode(ABC):
 
         offset = 2
         for _ in range(0, length):
-            current_message = input("Type the message to be sent as follows:\n" +
-                                    "<IP address> <subnet mask> <cost>\n")
-            current_message = current_message.strip().split(' ')
-
-            # keep trying for a good message
-            while len(current_message) != 3:
-                print("Invalid message")
-                current_message = input("<IP address> <subnet mask> <cost>\n")
-                current_message = current_message.strip().split(' ')
-
-            address = current_message[0].strip().split('.')
-
-            while len(address) != 4:
-                print("Invalid message")
-                current_message = input("<IP address> <subnet mask> <cost>\n")
-                current_message = current_message.strip().split(' ')
-                address = current_message[0].strip().split('.')
-
-            invalid_address_nums = [int(s) for s in address if int(s) > 255]
-            is_address_valid = len(invalid_address_nums) == 0
-            is_subnet_mask_valid = int(current_message[1]) < 32
-            is_cost_valid = int(current_message[2]) < 0xFFFFFF
-            is_input_valid = is_address_valid and is_cost_valid and is_subnet_mask_valid
-
-            while not is_input_valid:
-                print("Invalid message")
-                current_message = input("<IP address> <subnet mask> <cost>\n")
-                current_message = current_message.strip().split(' ')
-                address = current_message[0].strip().split('.')
-                invalid_address_nums = [int(s) for s in address if int(s) > 255]
-                is_address_valid = len(invalid_address_nums) == 0
-                is_subnet_mask_valid = int(current_message[1]) < 32
-                is_cost_valid = int(current_message[2]) < 0xFFFFFF
-                is_input_valid = is_address_valid and is_cost_valid and is_subnet_mask_valid
+            address, net_mask, cost = self.__get_valid_message_input()
 
             # Each triplet is encoded with the following 8-byte format:
             # BBBB (4 bytes) network address
@@ -147,17 +171,17 @@ class AbstractNode(ABC):
             #  I   (4 bytes) cost.
             #      The cost should only be 3 bytes, this is handled below.
             struct.pack_into('!BBBBB', message, offset,
-                            int(address[0]), int(address[1]),
-                            int(address[2]), int(address[3]),
-                            int(current_message[1]))
+                             address[0], address[1],
+                             address[2], address[3],
+                             net_mask)
 
             # Pack the cost into a 4 byte buffer
-            cost = struct.pack('!I', int(current_message[2]))
+            cost_bytes = struct.pack('!I', cost)
 
             # Write the cost into the message buffer, copying only 3 bytes and omitting 1
             # The least significant byte is the one omitted because its encoded
             # as big endian
-            message[offset+5:offset+8] = cost[1:]
+            message[offset+5:offset+8] = cost_bytes[1:]
 
             # Move the offset to write the next triplet
             offset += self.TRIPLET_SIZE
